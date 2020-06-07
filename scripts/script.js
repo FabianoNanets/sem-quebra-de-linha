406f0938ca62067799c3591ad7a88c3d76b8cba7
FabianoNanets <65836046+FabianoNanets@users.noreply.github.com>
2020-06-07 16:17:07 -0300

    ADD Funções

--- scripts/script.js
+++ scripts/script.js
@@ -17,14 +17,42 @@ function quebraLinha(){
   })  
 }
 
-function fazerCopia(){
-  document.getElementById('copiar').addEventListener('click', () =>{
-    document.getElementById('texto').select();
-    document.execCommand('copy');
-  });
+function fazerCopia() {
+  let range = document.createRange();
+  range.selectNode(document.getElementById("textoFinal"));
+  window.getSelection().removeAllRanges(); // limpa seleção atual
+  window.getSelection().addRange(range); // seleciona o texto
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();// limpa seleção
 }
 
 function limparTela(){
   document.getElementById("texto").value='';
+  textoFinal.innerHTML = '';
 }
 
+function fazerSelecao(){
+  var novoTexto = [];
+
+  var textoPego = document.getElementById('texto').value;
+  var arrayTexto = textoPego.split(' ');
+  
+  if(document.getElementById('palavra').value == "") {
+    var palavraAselecionar = document.querySelector('.garantia:checked').value;
+  } else {
+    var palavraAselecionar = document.getElementById('palavra').value;
+  }
+  
+
+  for (let i = 0; i < arrayTexto.length; i++) {
+    if (arrayTexto[i].toLowerCase() === palavraAselecionar.toLowerCase()){
+      var versaoNova = arrayTexto[i].toUpperCase().bold();
+      novoTexto.push(versaoNova);
+    } else {
+      novoTexto.push(arrayTexto[i]);
+   } 
+  }
+  
+  var novoTextoFinal = novoTexto.toString().replace(/,/gi, " ");
+  textoFinal.innerHTML = novoTextoFinal;
+}
