7798c968cb56028baa1f6e70b74312a7e5f26fe0
FabianoNanets <65836046+FabianoNanets@users.noreply.github.com>
2020-06-06 22:31:01 -0300

    ADD BUTTON CLEAR

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,30 @@
+function quebraLinha(){
+  let textarea = document.getElementById("texto");
+  let edit = document.getElementById("change");
 
-let textarea = document.getElementById("texto");
-let edit = document.getElementById("change");
+  edit.addEventListener("click", () => {
+    let texto = textarea.value;
+    let resultado = "";
 
-edit.addEventListener("click", () => {
-  let texto = textarea.value;
-  let resultado = "";
-  
-  for (let i = 0; i < texto.length; i++) {
-    texto.charCodeAt(i) == 10 ?
-    resultado += " " :
-    resultado += texto[i];
-  }
-  
-  console.log(resultado)
-  textarea.value = resultado;
-})
+    for (i = 0; i < texto.length; i++) {
+      texto.charCodeAt(i) == 10 ?
+      resultado += " " :
+      resultado += texto[i];
+    }
 
-document.getElementById('copiar').addEventListener('click', () =>{
-  document.getElementById('texto').select();
-  document.execCommand('copy');
-});
+    console.log(resultado)
+    textarea.value = resultado;
+  })  
+}
+
+function fazerCopia(){
+  document.getElementById('copiar').addEventListener('click', () =>{
+    document.getElementById('texto').select();
+    document.execCommand('copy');
+  });
+}
+
+function limparTela(){
+  document.getElementById("texto").value='';
+}
+
